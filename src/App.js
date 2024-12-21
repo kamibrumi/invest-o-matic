f90678ede095869710ad42155ca952a243ad3c75
cbrumar <cbrumar@alifehealth.com>
2024-12-21 15:18:31 -0500

    added the build files

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import BarChart from './components/BarChart';
+import LineChart from './components/LineChart';
 
 function App() {
   return (
     <div className="App">
-      <h1>Investomatic</h1>
+      <h1>Invest-o-matic</h1>
+      <BarChart />
+      <LineChart />
     </div>
   );
 }
